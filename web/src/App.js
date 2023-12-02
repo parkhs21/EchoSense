88e101ff0e5ee8175f3f1059992dc4c55d5db34d
헥스/박해승 <2001phs92@gmail.com>
2023-12-02 22:35:11 +0900

    feat: 녹음 진행 및 파일 다운로드

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react';
+import axios from 'axios';
 
-function App() {
+const RecordButton = () => {
+  const [recording, setRecording] = useState(false);
+  const [audioUrl, setAudioUrl] = useState(null);
+  const mediaRecorderRef = useRef(null);
+
+  const startRecording = async () => {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+
+      mediaRecorderRef.current = new MediaRecorder(stream, { mimeType: 'audio/webm' });
+      mediaRecorderRef.current.ondataavailable = handleDataAvailable;
+
+      mediaRecorderRef.current.start();
+      setRecording(true);
+    } else {
+      console.error('브라우저가 오디오 녹음을 지원하지 않습니다.');
+    }
+  };
+
+  const stopRecording = () => {
+    mediaRecorderRef.current.stop();
+    mediaRecorderRef.current.stream.getTracks().forEach(track => track.stop());
+    setRecording(false);
+  };
+
+  const handleDataAvailable = async (event) => {
+    if (event.data.size > 0) {
+      const audioBlob = new Blob([event.data], { type: 'audio/wav' });
+      const audioUrl = URL.createObjectURL(audioBlob);
+      setAudioUrl(audioUrl);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={recording ? stopRecording : startRecording}>
+        {recording ? '녹음 중지' : '녹음 시작'}
+      </button>
+      {audioUrl && <a href={audioUrl} download="recording.wav">다운로드</a>}
     </div>
   );
-}
+};
 
-export default App;
+export default RecordButton;
