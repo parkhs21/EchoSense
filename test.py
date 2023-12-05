55f6c20f35f59b8765d5c7301c8cc4cc0d91c67c
yesyes-12 <inbeom1202@gmail.com>
2023-12-05 18:15:03 +0900

    ignore수정

--- test.py
+++ test.py
@@ -1,171 +1,33 @@
-#import requests
-
-#url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=37.566535,126.97796919999996&radius=1000&type=restaurant&key=AIzaSyBETTT5fvImfLfRr86Xo4Y1ELWIBXV86NY"
-#payload={}
-#headers = {}
-
-#response = requests.request("GET", url, headers=headers, data=payload)
-
-#print(response.text)
-
-import json
-import openai
-from openai import OpenAI
+from langchain.retrievers.web_research import WebResearchRetriever
 import os
-import requests
+from langchain.vectorstores import Chroma 
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chat_models.openai import ChatOpenAI
+from langchain.utilities import GoogleSearchAPIWrapper
 
-client = OpenAI(api_key='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')
+os.environ["OPENAI_API_KEY"] = "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"
+os.environ["GOOGLE_CSE_ID"] = "f23f31fe730d14cb9"
+os.environ["GOOGLE_API_KEY"] = "AIzaSyC0RcGolyYn7ZoWM2eAPNWXnf-Wm46RDCc"
 
-def fetch_customer_profile(user_id):
-    # You can replace this with a real API call in the production code
-    if user_id == "user1234":
-        return {
-            "name": "John Doe",
-            "location": {
-                "latitude": 37.7955,
-                "longitude": -122.4026,
-            },
-            "preferences": {
-                "food": ["Italian", "Sushi"],
-                "activities": ["Hiking", "Reading"],
-            },
-            "behavioral_metrics": {
-                "app_usage": {
-                    "daily": 2,  # hours
-                    "weekly": 14  # hours
-                },
-                "favourite_post_categories": ["Nature", "Food", "Books"],
-                "active_time": "Evening",
-            },
-            "recent_searches": ["Italian restaurants nearby", "Book clubs"],
-            "recent_interactions": ["Liked a post about 'Best Pizzas in New York'", "Commented on a post about 'Central Park Trails'"],
-            "user_rank": "Gold",  # based on some internal ranking system
-        }
-    else:
-        return None
-def get_place_details(place_id, api_key):
-    URL = f"https://maps.googleapis.com/maps/api/place/details/json?place_id={place_id}&key={api_key}"
-    response = requests.get(URL)
-    if response.status_code == 200:
-        result = json.loads(response.content)["result"]
-        return result
-    else:
-        print(f"Google Place Details API request failed with status code {response.status_code}")
-        print(f"Response content: {response.content}")
-        return None
-def call_google_places_api(user_id, place_type, food_preference=None):
-    try:
-        # Fetch customer profile
-        customer_profile = fetch_customer_profile(user_id)
-        if customer_profile is None:
-            return "I couldn't find your profile. Could you please verify your user ID?"
+vector_store = Chroma(embedding_function=OpenAIEmbeddings(),
+                      persist_directory="./chroma_db_oai")
 
-        # Get location from customer profile
-        lat = customer_profile["location"]["latitude"]
-        lng = customer_profile["location"]["longitude"]
+search_llm = ChatOpenAI(temperature=0) # Search Query를 위한 LLM
 
-        API_KEY = 'AIzaSyBETTT5fvImfLfRr86Xo4Y1ELWIBXV86NY'  # retrieve API key from environment variable
-        LOCATION = f"{lat},{lng}"
-        RADIUS = 500  # search within a radius of 500 meters
-        TYPE = place_type
+search = GoogleSearchAPIWrapper() # SearchAPI Wrapper 객체 생성
 
-        # If the place_type is restaurant and food_preference is not None, include it in the API request
-        if place_type == 'restaurant' and food_preference:
-            URL = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={LOCATION}&radius={RADIUS}&type={TYPE}&keyword={food_preference}&key={API_KEY}"
-        else:
-            URL = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={LOCATION}&radius={RADIUS}&type={TYPE}&key={API_KEY}"
+# Web Research Retriever
+web_research_retriever = WebResearchRetriever.from_llm(
+    llm=search_llm,
+    vectorstore=vector_store,
+    search=search,
+)
 
-        response = requests.get(URL)
-        if response.status_code == 200:
-            results = json.loads(response.content)["results"]
-            places = []
-            for place in results[:2]:  # limit to top 2 results
-                place_id = place.get("place_id")
-                place_details = get_place_details(place_id, API_KEY)  # Get the details of the place
+from langchain.chains import RetrievalQAWithSourcesChain
+response_llm = ChatOpenAI(temperature=0.9)
+qa_chain = RetrievalQAWithSourcesChain.from_chain_type(response_llm, 
+                                                       retriever = web_research_retriever)
 
-                place_name = place_details.get("name", "N/A")
-                place_types = next((t for t in place_details.get("types", []) if t not in ["food", "point_of_interest"]), "N/A")  # Get the first type of the place, excluding "food" and "point_of_interest"
-                place_rating = place_details.get("rating", "N/A")  # Get the rating of the place
-                total_ratings = place_details.get("user_ratings_total", "N/A")  # Get the total number of ratings
-                place_address = place_details.get("vicinity", "N/A")  # Get the vicinity of the place
-
-                if ',' in place_address:  # If the address contains a comma
-                    street_address = place_address.split(',')[0]  # Split by comma and keep only the first part
-                else:
-                    street_address = place_address
-
-                # Prepare the output string for this place
-                place_info = f"{place_name} is a {place_types} located at {street_address}. It has a rating of {place_rating} based on {total_ratings} user reviews."
-
-                places.append(place_info)
-
-            return places
-        else:
-            print(f"Google Places API request failed with status code {response.status_code}")
-            print(f"Response content: {response.content}")  # print out the response content for debugging
-            return []
-    except Exception as e:
-        print(f"Error during the Google Places API call: {e}")
-        return []
-    
-def provide_user_specific_recommendations(user_input, user_id):
-    customer_profile = fetch_customer_profile(user_id)
-    if customer_profile is None:
-        return "I couldn't find your profile. Could you please verify your user ID?"
-
-    customer_profile_str = json.dumps(customer_profile)
-
-    food_preference = customer_profile.get('preferences', {}).get('food', [])[0] if customer_profile.get('preferences', {}).get('food') else None
-
-
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-    {
-        "role": "system",
-        "content": f"You are a sophisticated AI assistant, a specialist in user intent detection and interpretation. Your task is to perceive and respond to the user's needs, even when they're expressed in an indirect or direct manner. You excel in recognizing subtle cues: for example, if a user states they are 'hungry', you should assume they are seeking nearby dining options such as a restaurant or a cafe. If they indicate feeling 'tired', 'weary', or mention a long journey, interpret this as a request for accommodation options like hotels or guest houses. However, remember to navigate the fine line of interpretation and assumption: if a user's intent is unclear or can be interpreted in multiple ways, do not hesitate to politely ask for additional clarification. Make sure to tailor your responses to the user based on their preferences and past experiences which can be found here {customer_profile_str}"
-    },
-    {"role": "user", "content": user_input}
-],
-        temperature=0,
-        functions=[
-            {
-                "name": "call_google_places_api",
-                "description": "This function calls the Google Places API to find the top places of a specified type near a specific location. It can be used when a user expresses a need (e.g., feeling hungry or tired) or wants to find a certain type of place (e.g., restaurant or hotel).",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "place_type": {
-                            "type": "string",
-                            "description": "The type of place to search for."
-                        }
-                    }
-                },
-                "result": {
-                    "type": "array",
-                    "items": {
-                        "type": "string"
-                    }
-                }
-            }
-        ],
-    )
-
-    print(response.choices[0].message.function_call)
-
-    if response.choices[0].finish_reason=='function_call':
-        function_call = response.choices[0].message.function_call
-        if function_call.name == "call_google_places_api":
-            place_type = json.loads(function_call.arguments)["place_type"]
-            places = call_google_places_api(user_id, place_type, food_preference)
-            if places:  # If the list of places is not empty
-                return f"Here are some places you might be interested in: {' '.join(places)}"
-            else:
-                return "I couldn't find any places of interest nearby."
-
-    return "I am sorry, but I could not understand your request."
-
-user_id = "user1234"
-user_input = "I'm hungry"
-output = provide_user_specific_recommendations(user_input, user_id)
-print(output)
+user_input = "When is Newzine's Minji's birthday??"
+result = qa_chain({"question": user_input})
+print(result)
